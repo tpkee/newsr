df2832329ff6ef025bc635403c47ba6495492eba
tpkee <89136677+tpkee@users.noreply.github.com>
2023-01-17 21:58:06 +0100

    Feat: added tailwindcss config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,5 +6,9 @@ export default defineNuxtConfig({
 
     experimental: {
         reactivityTransform: true
-    }
+    },
+
+    modules: [
+      '@nuxtjs/tailwindcss'
+    ]
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <nuxt-welcome />
+        <div class="text-2xl">lol</div>
     </div>
 </template>
 
